205ccf03304edf597868a9ab521d2369f8c018cf
Ken Sodemann <ken@ionic.io>
2021-01-28 10:44:35 -0600

    attempt to replicate the issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  pct = 0;
 
   constructor() {}
 
+  ngOnInit() {
+    let i = setInterval(() => {
+      if (this.pct < 1.0) {
+        this.pct += 0.01;
+      } else {
+        clearInterval(i);
+      }
+    }, 40);
+  }
 }
